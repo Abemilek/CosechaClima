daef103d9dc6d1fa4f2267d4f4d7667e9676d9cb
Abemilek <abemilek@proton.me>
2026-07-27 23:23:10 -0600

    fix(decision-engine): correct canicula calculation and add SinRiesgo state

---
+++ backend/WebApi.Models/EventoClimaticoId.cs
@@ -0,0 +1,13 @@
+namespace WebApi.Models;
+
+public enum EventoClimaticoId
+{
+
+    // si el insert s cambia en el futuro, tambien se debe cambiar este orden
+    LluviaIntensa = 1,
+    Canicula = 2,
+    VientoFuerte = 3,
+    TemperaturaExtrema = 4,
+    RiesgoHelada = 5,
+    SinRiesgo = 6
+}
--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddScoped<ConnectionBD>();
 // cada servicio registrado, por cada interfaz que exista
 //builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IParcelaService, ParcelaService>();
-//builder.Services.AddScoped<IUmbralConfiguracionService, UmbralConfiguracionService>();
+builder.Services.AddScoped<IUmbralConfiguracionService, UmbralConfiguracionService>();
 builder.Services.AddScoped<IDatosClimaticoService, DatosClimaticoService>();
 builder.Services.AddScoped<IAlertaService, AlertaService>();
 //builder.Services.AddScoped<IBitacoraService, BitacoraService>();
