165d6858ab667b56eda7e1bf4437a2b70436b165
Abemilek <abemilek@proton.me>
2026-07-27 20:07:47 -0600

    feat(services): add ParcelaService, UmbralConfiguracionService, DatosClimaticoService

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddScoped<ConnectionBD>();
 //builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IParcelaService, ParcelaService>();
 //builder.Services.AddScoped<IUmbralConfiguracionService, UmbralConfiguracionService>();
-//builder.Services.AddScoped<IDatosClimaticoService, DatosClimaticoService>();
+builder.Services.AddScoped<IDatosClimaticoService, DatosClimaticosService>();
 builder.Services.AddScoped<IAlertaService, AlertaService>();
 //builder.Services.AddScoped<IBitacoraService, BitacoraService>();
 builder.Services.AddScoped<IMotorDecisionesService, MotorDecisionesService>();
