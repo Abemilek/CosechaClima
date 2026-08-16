a5f59922a8079371d28d461465fcaaf967a2178a
Abemilek <abemilek@proton.me>
2026-08-15 22:39:59 -0600

    fix(datos-climaticos): restore GuardarOActualizar upsert method

--- backend/Services/WebApi.Interface/IDatosClimaticoService.cs
+++ backend/Services/WebApi.Interface/IDatosClimaticoService.cs
@@ -4,6 +4,7 @@ namespace WebApi.Interface;
 
 public interface IDatosClimaticoService {
     Task<int> GuardarDatos (DatosClimaticos datos);
+    Task<int> GuardarOActualizar (DatosClimaticos datos);
     Task<DatosClimaticos> ObtenerPorParcelaYFecha(int parcelaId, DateTime fecha);
     Task<List<DatosClimaticos>> ObtenerUltimosDatos (int parcelaId, int dias = 7);
     Task<List<DatosClimaticos>> ObtenerPorRangoFechas (int parcelaId, DateTime desde, DateTime hasta);
